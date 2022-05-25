709452e2237351810509f04598d699bb7d4fb228
morrssikk <91206206+morrssikk@users.noreply.github.com>
2022-05-25 22:59:52 +0300

    Добавьте файлы проекта.

---
+++ 13,1/13,1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+#include <iomanip>
+
+#include "dod.h"
+#include "sum.h"
+#include "var.h"
+
+using namespace std;
+using namespace nsDod;
+using namespace nsSum;
+using namespace nsVar;
+
+
+	int main()
+	{
+		cout << "x_p = "; cin >> x_p;
+		cout << "x_k = "; cin >> x_k;
+		cout << "dx = "; cin >> dx;
+		cout << "e = "; cin >> e;
+		cout << endl;
+		x = x_p;
+		while (x <= x_k) {
+			sum(); // виклик процедури обчислення суми
+			cout << x << " "
+				<< s << " "
+				<< exp(x) << " "
+				<< n << endl; // тут слід використовувати форматний вивід
+			x += dx;
+		}
+		cin.get();
+		return 0;
+	}
---
+++ 13,1/dod.cpp
@@ -0,0 +1,6 @@
+#include "dod.h"
+#include "var.h" // підключили зовнішні оголошення змінних
+using namespace nsVar;
+void nsDod::dod() {
+	a *= (2 * n - 1) / ((2 * n + 1) * x * x); // доданок домножується на
+}
